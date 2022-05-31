8888e094c7be6eac7e19d9f11c4ea235b4552d0d
Mironov Ury <mironoff2007@rambler.ru>
2022-05-31 23:24:43 +0300

    coordinates generator task

---
+++ app/src/main/java/ru/mironov/showrandomcoordinatesonmap/ui/main/model/CoordinatesGenerator.kt
@@ -0,0 +1,10 @@
+package ru.mironov.showrandomcoordinatesonmap.ui.main.model
+
+class CoordinatesGenerator {
+
+    fun generateCoordinates(): Pair<Double, Double>{
+        //тут реализовать генератор
+    }
+
+
+}
---
+++ app/src/test/java/ru/mironov/showrandomcoordinatesonmap/CoordinatesGeneratorTest.kt
@@ -0,0 +1,15 @@
+package ru.mironov.showrandomcoordinatesonmap
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class CoordinatesGeneratorTest {
+    @Test
+    fun test() {
+        // Тут сгенерировать лист из 100 пар, например и проверить, что все координаты в верных границах
+        // Тест сам по себе бесполезный, но для тренировки норм
+        assertEquals()
+    }
+}
