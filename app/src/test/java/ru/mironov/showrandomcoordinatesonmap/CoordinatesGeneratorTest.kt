a03b4cb93726da6afb39946c255c8d4514826292
Mironov Ury <mironoff2007@rambler.ru>
2022-06-02 17:34:56 +0300

    correct test

--- app/src/test/java/ru/mironov/showrandomcoordinatesonmap/CoordinatesGeneratorTest.kt
+++ app/src/test/java/ru/mironov/showrandomcoordinatesonmap/CoordinatesGeneratorTest.kt
@@ -1,10 +1,7 @@
 package ru.mironov.showrandomcoordinatesonmap
 
-import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import ru.mironov.showrandomcoordinatesonmap.ui.main.model.CoordinatesGenerator
-import java.util.*
-import kotlin.random.Random
 
 
 class CoordinatesGeneratorTest() {
@@ -13,21 +10,19 @@ class CoordinatesGeneratorTest() {
 
     @Test
     fun test() {
-        // Тут сгенерировать лист из 100 пар, например и проверить, что все координаты в верных границах
-        // Тест сам по себе бесполезный, но для тренировки норм
-        val clearMuteList: MutableList<Pair<Double, Double>> = mutableListOf()
+        val coordinatesList: MutableList<Pair<Double, Double>> = mutableListOf()
+
         repeat(99) {
-            clearMuteList.add(generator.generateCoordinates())
+            coordinatesList.add(generator.generateCoordinates())
         }
 
-        var simpleBoolean = false
+        var allAreInRange = true
 
-        clearMuteList.forEach {
-            if (it.first in -90.0..90.0 && it.second in -180.0..180.0)
-                simpleBoolean = true
-
-            assert(simpleBoolean)
+        coordinatesList.forEach {
+            if (it.first !in -90.0..90.0 || it.second !in -180.0..180.0) allAreInRange = false
         }
+
+        assert(allAreInRange)
     }
 }
 
