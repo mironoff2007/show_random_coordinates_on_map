2c9a029d57e172d1c83d937e06cf2a9ea370c8de
Mironov Ury <mironoff2007@rambler.ru>
2022-06-02 12:16:29 +0300

    delete updates from push

--- app/src/main/java/ru/mironov/showrandomcoordinatesonmap/ui/main/model/CoordinatesGenerator.kt
+++ app/src/main/java/ru/mironov/showrandomcoordinatesonmap/ui/main/model/CoordinatesGenerator.kt
@@ -7,16 +7,7 @@ class CoordinatesGenerator {
 
     fun generateCoordinates(): Pair<Double, Double> {
         //тут реализовать генератор
-        //широта
-        val randLatValue: Double = Random.nextDouble(-90.0, 90.1)
-        val randLatValueRounded = String.format(Locale.US, "%.4f", randLatValue)
-        val lat = randLatValueRounded.toDouble()
-        //долгота
-        val randLonValue: Double = Random.nextDouble(-180.0, 180.1)
-        val randLonValueRounded = String.format(Locale.US, "%.4f", randLonValue)
-        val lon = randLonValueRounded.toDouble()
-        //Возвращает пару широты/долготы в формате (0.0000, 0.0000)
-        return Pair(lat, lon)
+
     }
 
 }
--- app/src/test/java/ru/mironov/showrandomcoordinatesonmap/CoordinatesGeneratorTest.kt
+++ app/src/test/java/ru/mironov/showrandomcoordinatesonmap/CoordinatesGeneratorTest.kt
@@ -12,27 +12,9 @@ class CoordinatesGeneratorTest() {
     fun test() {
         // Тут сгенерировать лист из 100 пар, например и проверить, что все координаты в верных границах
         // Тест сам по себе бесполезный, но для тренировки норм
-        val checkList = mutableListOf(listOf(randomPairGenerator()))
-        val clearMuteList: MutableList<List<Pair<Double, Double>>> = mutableListOf()
 
-        repeat(99) {
-            clearMuteList.add((listOf(randomPairGenerator())))
-        }
-        assertEquals(checkList, clearMuteList)
     }
 
-    fun randomPairGenerator(): Pair<Double, Double> {
-        //расчет широты
-        val randLatValue: Double = Random.nextDouble(-90.0, 90.1)
-        val randLatValueRounded = String.format(Locale.US, "%.4f", randLatValue)
-        val lat = randLatValueRounded.toDouble()
-        //расчет долготы
-        val randLonValue: Double = Random.nextDouble(-180.0, 180.1)
-        val randLonValueRounded = String.format(Locale.US, "%.4f", randLonValue)
-        val lon = randLonValueRounded.toDouble()
-        //Возвращает пару широты/долготы в формате (0.0000, 0.0000)
-        return Pair(lat, lon)
-    }
 }
 
 
