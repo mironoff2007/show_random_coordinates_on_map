9375f05ee11f85695479dd2a2dffeaad3aa696ba
Erkesh <dedeeverkesh@gmail.com>
2022-06-02 16:29:04 +0300

    Test remade

--- app/src/test/java/ru/mironov/showrandomcoordinatesonmap/CoordinatesGeneratorTest.kt
+++ app/src/test/java/ru/mironov/showrandomcoordinatesonmap/CoordinatesGeneratorTest.kt
@@ -2,20 +2,39 @@ package ru.mironov.showrandomcoordinatesonmap
 
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import ru.mironov.showrandomcoordinatesonmap.ui.main.model.CoordinatesGenerator
 import java.util.*
 import kotlin.random.Random
 
 
 class CoordinatesGeneratorTest() {
 
+    private val generator = CoordinatesGenerator()
+
     @Test
     fun test() {
         // Тут сгенерировать лист из 100 пар, например и проверить, что все координаты в верных границах
         // Тест сам по себе бесполезный, но для тренировки норм
+        val clearMuteList: MutableList<Pair<Double, Double>> = mutableListOf()
+        repeat(99) {
+            clearMuteList.add(generator.generateCoordinates())
+        }
 
+        var simpleBoolean: Boolean
+
+        val vDiapazone = clearMuteList.forEach {
+            if (it.first in -90.0000..90.0001 || it.second in -180.0000..180.0001)
+                simpleBoolean = true
+        }
+        val neVDiapazone = clearMuteList.forEach {
+            if (it.first !in -90.0000..90.0001 || it.second !in -180.0000..180.0001)
+                simpleBoolean = false
+        }
+        assertEquals(vDiapazone, neVDiapazone)
     }
 
 }
+
 
 
 
