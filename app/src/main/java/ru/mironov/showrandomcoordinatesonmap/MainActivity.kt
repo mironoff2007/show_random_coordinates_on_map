de7f87f7dfb4731d24b267cbd90840c57729399a
Erkesh <dedeeverkesh@gmail.com>
2022-06-09 19:06:22 +0300

    Added service

--- app/src/main/java/ru/mironov/showrandomcoordinatesonmap/MainActivity.kt
+++ app/src/main/java/ru/mironov/showrandomcoordinatesonmap/MainActivity.kt
@@ -1,7 +1,9 @@
 package ru.mironov.showrandomcoordinatesonmap
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.mironov.showrandomcoordinatesonmap.ui.main.GenService
 import ru.mironov.showrandomcoordinatesonmap.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,12 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+            startGen()
         }
+    }
+
+     fun startGen() {
+        startService(Intent(this,
+            GenService::class.java))
     }
 }
--- app/src/main/java/ru/mironov/showrandomcoordinatesonmap/ui/main/MainFragment.kt
+++ app/src/main/java/ru/mironov/showrandomcoordinatesonmap/ui/main/MainFragment.kt
@@ -1,14 +1,27 @@
 package ru.mironov.showrandomcoordinatesonmap.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.hardware.SensorManager
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProvider
+import ru.mironov.showrandomcoordinatesonmap.MainActivity
 import ru.mironov.showrandomcoordinatesonmap.R
+import ru.mironov.showrandomcoordinatesonmap.databinding.MainFragmentBinding
+
 
 class MainFragment : Fragment() {
+
+    private val gen = GenService()
+    private val mainActivity = MainActivity()
+    lateinit var binding: MainFragmentBinding
 
     companion object {
         fun newInstance() = MainFragment()
@@ -18,9 +31,16 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            mainActivity.startGen()
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
